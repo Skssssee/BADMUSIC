db9a2576d9fdd13b6499ed894df19b8dc2bb5fa9
ʙᴀᴅ ᴍᴜɴᴅᴀ <sukhwinderwarval50@gmail.com>
2025-07-12 12:14:38 +0530

    Edit config.py

--- config/config.py
+++ config/config.py
@@ -34,7 +34,7 @@ UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
     "https://github.com/Pbx-Official/BADMUSIC",
 )
-UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Bad")
+UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
