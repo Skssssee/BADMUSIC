9e66e24b336255144f61dfe231e1ceff4fc25242
hccccrr <vrajiv830@gmail.com>
2025-11-09 18:21:41 +0530

    Update userbot.py

--- BADMUSIC/core/userbot.py
+++ BADMUSIC/core/userbot.py
@@ -5,12 +5,6 @@ from BADMUSIC import config, logger
 
 class Userbot(Client):
     def __init__(self):
-        """
-        Initializes the userbot with multiple clients.
-
-        This method sets up clients for the userbot using predefined session strings.
-        Each client is assigned a unique name based on the key in the `clients` dictionary.
-        """
         self.clients = []
         clients = {"one": "SESSION1", "two": "SESSION2", "three": "SESSION3"}
         for key, string_key in clients.items():
@@ -28,14 +22,6 @@ class Userbot(Client):
             )
 
     async def boot_client(self, num: int, ub: Client):
-        """
-        Boot a client and perform initial setup.
-        Args:
-            num (int): The client number to boot (1, 2, or 3).
-            ub (Client): The userbot client instance.
-        Raises:
-            SystemExit: If the client fails to send a message in the log group.
-        """
         clients = {
             1: self.one,
             2: self.two,
@@ -43,27 +29,27 @@ class Userbot(Client):
         }
         client = clients[num]
         await client.start()
-        try:
-            await client.send_message(config.LOGGER_ID, "Assistant Started")
-        except:
-            raise SystemExit(f"Assistant {num} failed to send message in log group.")
-
-        client.id = ub.me.id
-        client.name = ub.me.first_name
-        client.username = ub.me.username
-        client.mention = ub.me.mention
+        client.id = client.me.id
+        client.name = client.me.first_name
+        client.username = client.me.username
+        client.mention = client.me.mention
         self.clients.append(client)
         try:
-            await ub.join_chat("PBX_CHAT")
-            await ub.join_chat("PBX_UPDATE")
+            await client.send_message(
+                config.LOGGER_ID,
+                f"❖<b> {client.mention} ᴀꜱꜱɪꜱᴛᴀɴᴛ {num} sᴛᴀʀᴛᴇᴅ</b>\n\n● ɪᴅ ➥ <code>{client.id}</code>\n● ɴᴀᴍᴇ ➥ {client.name}\n● ᴜsᴇʀɴᴀᴍᴇ ➥ @{client.username}",
+            )
+        except:
+            raise SystemExit(f"❖ ᴀꜱꜱɪꜱᴛᴀɴᴛ {num} ꜰᴀɪʟᴇᴅ ᴛᴏ ꜱᴇɴᴅ ᴍᴇꜱꜱᴀɢᴇ ɪɴ ʟᴏɢ ɢʀᴏᴜᴘ😥")
+
+        try:
+            await client.join_chat("PBX_CHAT")
+            await client.join_chat("PBX_UPDATE")
         except:
             pass
-        logger.info(f"Assistant {num} started as @{client.username}")
+        logger.info(f"❖ ᴀꜱꜱɪꜱᴛᴀɴᴛ {num} ꜱᴛᴀʀᴛᴇᴅ ᴀꜱ @{client.username}")
 
     async def boot(self):
-        """
-        Asynchronously starts the assistants.
-        """
         if config.SESSION1:
             await self.boot_client(1, self.one)
         if config.SESSION2:
@@ -72,13 +58,10 @@ class Userbot(Client):
             await self.boot_client(3, self.three)
 
     async def exit(self):
-        """
-        Asynchronously stops the assistants.
-        """
         if config.SESSION1:
             await self.one.stop()
         if config.SESSION2:
             await self.two.stop()
         if config.SESSION3:
             await self.three.stop()
-        logger.info("Assistants stopped.")
+        logger.info("❖ ꜱᴛᴏᴘᴘɪɴɢ ᴀꜱꜱɪꜱᴛᴀɴᴛꜱ 😥")
