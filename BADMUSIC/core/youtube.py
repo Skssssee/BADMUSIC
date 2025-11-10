d97ca065128d6aefed3e2413fc63de7a13d2b61a
hccccrr <vrajiv830@gmail.com>
2025-11-10 12:47:03 +0530

    Update youtube.py

--- BADMUSIC/core/youtube.py
+++ BADMUSIC/core/youtube.py
@@ -123,7 +123,7 @@ class YouTube:
             return fname
 
         except Exception as e:
-            logging.error(f"Error downloading TG media: {e}")
+            logging.error(f"ᴇʀʀᴏʀ ᴅᴏᴡɴʟᴏᴀᴅɪɴɢ ᴛɢ ᴍᴇᴅɪᴀ: {e}")
             return None
 
     def valid(self, url: str) -> bool:
@@ -239,7 +239,7 @@ class YouTube:
         song_data = await self.fetch_song(query, streamtype)
 
         if not song_data or "error" in song_data or "link" not in song_data:
-            error_msg = song_data.get("error", "Failed to process query")
+            error_msg = song_data.get("error", "ꜰᴀɪʟᴇᴅ ᴛᴏ ᴘʀᴏᴄᴇꜱꜱ Qᴜᴇʀʏ")
             return None, None, error_msg
 
         song_url = song_data["link"]
